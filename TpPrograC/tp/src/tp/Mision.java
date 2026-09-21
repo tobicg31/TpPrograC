d6b47cc841e56f38b08557b94ffebb8d7892724e
tobicg31 <82407605+tobicg31@users.noreply.github.com>
2026-09-21 20:36:30 -0300

    consultas de las misiones

--- TpPrograC/tp/src/tp/Mision.java
+++ TpPrograC/tp/src/tp/Mision.java
@@ -1,18 +1,21 @@
 package tp;
 
-public abstract class Mision {
+public abstract class Mision { //preguntar que hace bien cada mision y el paso a paso
     
 
     public void preparar(Nave n){
-        
+        System.out.println("preparo la nave...");
     }
     public void ejecutar(Nave n){
 
     }
     public void evaluar(Nave n){
-
+        /*
+        n.setCombustible() = n.getCombustible()-4;
+        n.setDesgaste() = n.setDesgaste()+4;
+        */
     }
     public void cerrar(Nave n){
-        
+        //aca iria el informe, creo(?
     }
 }
