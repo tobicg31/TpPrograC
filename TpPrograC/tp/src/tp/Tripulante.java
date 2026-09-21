ed3282aad49006af3199d54af2be90bcc776d583
Juano <juan2005.JCP@gmail.com>
2026-09-21 18:05:36 -0300

    Agrego constructor y getters de Tripulantes

--- TpPrograC/tp/src/tp/Tripulante.java
+++ TpPrograC/tp/src/tp/Tripulante.java
@@ -1,9 +1,44 @@
 package tp;
 
 public abstract class Tripulante {
-    private String identidad, cargo, planeta, antiguedad;
+    private String identidad, cargo, planeta;
+    private int antiguedad;
     
-    public Tripulante() {
-        super();
+    public Tripulante(String id,String cargo,String planeta,int antiguedad) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("El ID del tripulante no puede ser nulo ni vacío.");
+        }
+        if (cargo == null || planeta == null) {
+            throw new IllegalArgumentException("El cargo y planeta son obligatorios.");
+        }
+        if (antiguedad < 0) {
+            throw new IllegalArgumentException("La antigüedad no puede ser negativa.");
+        }
+
+        this.identidad=id;
+        this.cargo=cargo;
+        this.planeta=planeta;
+        this.antiguedad=antiguedad;
+    }
+
+    public String getIdentidad() {
+        return identidad;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getPlaneta() {
+        return planeta;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public String tooString(){
+        return (this.identidad+": Cargo:"+this.cargo+" |Planeta:"+this.planeta+" |Antiguedad:"+this.antiguedad);
+        
     }
 }
